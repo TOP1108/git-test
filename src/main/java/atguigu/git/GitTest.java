2dc942647880b409d4c761da33816a1ffc067cd8
TOP1108 <674176780@qq.com>
2022-03-27 00:15:16 +0800

    the fist commit

---
+++ src/main/java/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package atguigu.git;
+
+/**
+ * @Author: lzh
+ * @Date: 2022/3/27 0:10
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
